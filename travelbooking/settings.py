006bceab73e77088fe4cb39988bf181abfc6e86e
Lingan-07 <lingan2004@gmail.com>
2025-08-29 12:55:51 +0530

    updated host

--- travelbooking/settings.py
+++ travelbooking/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY","dev-key")
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lingan07.pythonanywhere.com']
 ROOT_URLCONF = "travelbooking.urls"
 
 
